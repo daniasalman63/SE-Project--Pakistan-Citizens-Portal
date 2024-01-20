9255b40ca6d6933402918a2e048e598f5049d386
daniasalman63 <74529986+daniasalman63@users.noreply.github.com>
2024-01-20 22:59:49 +0500

    Delete mysite directory

--- mysite/voting/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Candidate(models.Model):
-    product_id=models.AutoField
-    candidate_name=models.CharField(max_length=50)
-    number_of_votes=models.IntegerField()
-
-    def __str__(self):
-        return self.candidate_name
